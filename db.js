10beb71bcb8a1d57fcd389db3f1a1377a8c2a44b
rajkumarekkaladevi <rajkumarekkaladevi@gmail.com>
2026-06-08 17:32:27 +0530

    Connected Neon database

--- db.js
+++ db.js
@@ -2,14 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
+
 pool.connect()
-  .then(() => console.log('✅ Database Connected Successfully'))
-  .catch(err => console.error('❌ Database Connection Failed:', err.message));
+    .then(() => console.log('✅ Neon Database Connected Successfully'))
+    .catch(err => console.error('❌ Database Connection Failed:', err.message));
